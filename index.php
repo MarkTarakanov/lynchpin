96a56ad419c741ef524f53bbc422a67e1df0593d
Mark Tarakanov <Mark.Tarakan@gmail.com>
2016-05-01 16:13:13 +0100

    Added index!!! =D :sob:

---
+++ index.php
@@ -0,0 +1,45 @@
+<Docktype html>
+<html>
+<head>
+<body>
+  <?php
+  if (isset($_POST['submit'])) {
+    //process form
+      printf('User name: %s
+      <br> Password: %s
+      <br> Gender: %s
+      <br> Color: %s
+      <br> Language(s): %s,
+      <br>T&amp;C: %s',
+      htmlspecialchars($_POST['name']),
+      htmlspecialchars($_POST['password']),
+      htmlspecialchars($_POST['gender']),
+      htmlspecialchars($_POST['color']),
+      htmlspecialchars(implode(' ', $_POST['languages'])),
+      htmlspecialchars($_POST['tc']));
+  }
+  ?>
+  <form method="post" action="">
+    User Name: <input type="text" name="name"><br>
+    Password: <input type="password" name="password"><br>
+    Gender:
+      <input type="radio" name="gender" value="f">female
+      <input type="radio" name="gender" value="m">male <br>
+      Favorite color:
+        <select name="color">
+            <option value="#f00">red</option>
+            <option value="#0f0">green</option>
+            <option value="#00f">blue</option>
+          </select><br>
+      Languages spoken:
+        <select name="languages[]" multiple size="3">
+          <option value="en">English</option>
+          <option value="fr">French</option>
+          <option value="it">Italian</option>
+        </select><br>
+        <input type="checkbox" name="tc" value="ok">I accept the T&C<br>
+        <input type="submit" name="submit" value="Submit">
+</form>
+</body>
+</head>
+</html>
