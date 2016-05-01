7bdbfc7fce67180711f89b3fc883cf06617d3279
Daniil Tarakanov <daniil@tarakanov.me>
2016-05-01 17:49:06 +0200

    Established SQL connectivity.

--- index.php
+++ index.php
@@ -5,18 +5,47 @@
   <?php
   if (isset($_POST['submit'])) {
     //process form
-      printf('User name: %s
-      <br> Password: %s
-      <br> Gender: %s
-      <br> Color: %s
-      <br> Language(s): %s,
-      <br>T&amp;C: %s',
-      htmlspecialchars($_POST['name']),
-      htmlspecialchars($_POST['password']),
-      htmlspecialchars($_POST['gender']),
-      htmlspecialchars($_POST['color']),
-      htmlspecialchars(implode(' ', $_POST['languages'])),
-      htmlspecialchars($_POST['tc']));
+      $name = $_POST['name'];
+      $password = $_POST['password'];
+      $gender = $_POST['gender'];
+      $color = $_POST['color'];
+      $languages = $_POST['languages'];
+
+      // Connect to DB
+      $mysqli = new mysqli('localhost', 'Users', 'topsecret', 'Mark Tarakanov');
+
+      // Throw exception instead
+      if ($this->$mysqli->connect_errno) {
+          echo 'Connect failed: ', $this->$mysqli->connect_error, '" }';
+          exit();
+      }
+
+      // Check if user exists
+      $sql = "SELECT id FROM Users WHERE username='$name'";
+      $result = $mysqli->query($sql);
+
+      if (mysqli_num_rows($result) > 0)
+      {
+          $message = [0 => "This user already exists!"];
+      }
+      else
+      {
+        $sql = "INSERT INTO Users (username, password, gender, color, languages) VALUES ('$name', '$password', '$gender', '$color', '$languages')";
+        $result = $mysqli->query($sql);
+
+        printf('User name: %s
+        <br> Password: %s
+        <br> Gender: %s
+        <br> Color: %s
+        <br> Language(s): %s,
+        <br>T&amp;C: %s',
+        htmlspecialchars($_POST['name']),
+        htmlspecialchars($_POST['password']),
+        htmlspecialchars($_POST['gender']),
+        htmlspecialchars($_POST['color']),
+        htmlspecialchars(implode(' ', $_POST['languages'])),
+        htmlspecialchars($_POST['tc']));
+      }
   }
   ?>
   <form method="post" action="">
