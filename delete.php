074dc06f5554eba8a925a43d5dcd81910fd5782c
Mark Tarakanov <Mark.Tarakan@gmail.com>
2016-05-13 09:41:42 +0100

    Some more changes.

---
+++ delete.php
@@ -0,0 +1,28 @@
+<?php
+  if (isset($_GET['id']) && ctype_digit($_GET['id'])) {
+    $id = $_GET['id'];
+  } else {
+    header('Location: select.php');
+  }
+
+
+ ?>
+
+
+
+<!DOCKTYPE>
+<html>
+<head>
+    <title>PHP</title>
+</head>
+<body>
+<?php
+          $db = mysqli_connect('localhost', 'Mark Tarakanov', 'topsecret', 'Users');
+          $sql = "DELETE FROM Users WHERE id=$id";
+          mysqli_query($db, $sql);
+          echo '<p>User deleted.</p>';
+          mysqli_close($db);
+
+ ?>
+</body>
+</html>
